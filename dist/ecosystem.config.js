10db33fbd5b4400d7e3b231317b8b5d4e93f55cf
Emmanuel Osei Mensah <mancuniamoe@gmail.com>
2026-06-28 00:58:25 +0000

    update: added doppler

---
+++ dist/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "anansesemfie_api",
+      script: "index.js", // or index.js, dist/main.js, etc.
+      interpreter: "node",
+      instances: 1, // or "max" for cluster mode
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
